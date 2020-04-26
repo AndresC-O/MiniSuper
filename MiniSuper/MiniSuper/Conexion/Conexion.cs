d7afd2db066c4c7ce8df13dcf0a9029b056660b9
AndresC-O <62961669+AndresC-O@users.noreply.github.com>
2020-04-25 20:29:49 -0600

    Creando Conexion.cs
    En este "commit", se trabajó la parte de la creación de la cadena de conexión, donde nos estaremos conectando al servidor.

---
+++ MiniSuper/MiniSuper/Conexion/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MiniSuper.Conexion
+{
+    public class Conexion
+    {
+        //Método que nos servirá para conectarnos a nuestro servidor donde se encuentra la base de datos.
+        public static IDbConnection Conectar()
+        {
+            String cadena = "Data Source=.;Initial Catalog=MiniSuper;Integrated Security=True";
+            SqlConnection cn = new SqlConnection(cadena);
+            IDbConnection conexion = cn;
+            return conexion;
+        }
+    }
+}

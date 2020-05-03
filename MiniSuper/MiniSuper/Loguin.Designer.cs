6e42905cd45c61c0a8269e09f532957c16f71fc4
AndresC-O <62961669+AndresC-O@users.noreply.github.com>
2020-05-03 12:11:08 -0600

    Validaciones en el FormLoguin

--- MiniSuper/MiniSuper/Loguin.Designer.cs
+++ MiniSuper/MiniSuper/Loguin.Designer.cs
@@ -31,68 +31,88 @@
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.Label contraseniaLabel;
             System.Windows.Forms.Label usuarioLabel;
-            this.contraseniaTextBox = new System.Windows.Forms.TextBox();
-            this.usuarioTextBox = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.Label label1;
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
             this.btnAcceder = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
             this.empleadosBindingSource = new System.Windows.Forms.BindingSource(this.components);
             contraseniaLabel = new System.Windows.Forms.Label();
             usuarioLabel = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.empleadosBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // contraseniaLabel
             // 
             contraseniaLabel.AutoSize = true;
-            contraseniaLabel.Location = new System.Drawing.Point(191, 256);
+            contraseniaLabel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            contraseniaLabel.Location = new System.Drawing.Point(48, 156);
             contraseniaLabel.Name = "contraseniaLabel";
-            contraseniaLabel.Size = new System.Drawing.Size(65, 13);
-            contraseniaLabel.TabIndex = 5;
-            contraseniaLabel.Text = "contrasenia:";
-            // 
-            // contraseniaTextBox
-            // 
-            this.contraseniaTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.empleadosBindingSource, "contrasenia", true));
-            this.contraseniaTextBox.Location = new System.Drawing.Point(298, 253);
-            this.contraseniaTextBox.Name = "contraseniaTextBox";
-            this.contraseniaTextBox.Size = new System.Drawing.Size(100, 20);
-            this.contraseniaTextBox.TabIndex = 6;
+            contraseniaLabel.Size = new System.Drawing.Size(124, 22);
+            contraseniaLabel.TabIndex = 3;
+            contraseniaLabel.Text = "Contraseña:";
             // 
             // usuarioLabel
             // 
             usuarioLabel.AutoSize = true;
-            usuarioLabel.Location = new System.Drawing.Point(191, 193);
+            usuarioLabel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            usuarioLabel.Location = new System.Drawing.Point(48, 84);
             usuarioLabel.Name = "usuarioLabel";
-            usuarioLabel.Size = new System.Drawing.Size(44, 13);
-            usuarioLabel.TabIndex = 17;
-            usuarioLabel.Text = "usuario:";
+            usuarioLabel.Size = new System.Drawing.Size(80, 22);
+            usuarioLabel.TabIndex = 1;
+            usuarioLabel.Text = "Usuario:";
             // 
-            // usuarioTextBox
+            // txtContra
             // 
-            this.usuarioTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.empleadosBindingSource, "usuario", true));
-            this.usuarioTextBox.Location = new System.Drawing.Point(298, 190);
-            this.usuarioTextBox.Name = "usuarioTextBox";
-            this.usuarioTextBox.Size = new System.Drawing.Size(100, 20);
-            this.usuarioTextBox.TabIndex = 18;
+            this.txtContra.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.empleadosBindingSource, "contrasenia", true));
+            this.txtContra.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContra.Location = new System.Drawing.Point(52, 181);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.PasswordChar = '·';
+            this.txtContra.Size = new System.Drawing.Size(216, 31);
+            this.txtContra.TabIndex = 4;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.empleadosBindingSource, "usuario", true));
+            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(52, 109);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(216, 31);
+            this.txtUsuario.TabIndex = 2;
             // 
             // btnAcceder
             // 
-            this.btnAcceder.Location = new System.Drawing.Point(194, 377);
+            this.btnAcceder.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcceder.Location = new System.Drawing.Point(52, 241);
             this.btnAcceder.Name = "btnAcceder";
-            this.btnAcceder.Size = new System.Drawing.Size(75, 23);
-            this.btnAcceder.TabIndex = 19;
+            this.btnAcceder.Size = new System.Drawing.Size(216, 41);
+            this.btnAcceder.TabIndex = 5;
             this.btnAcceder.Text = "Acceder";
             this.btnAcceder.UseVisualStyleBackColor = true;
             this.btnAcceder.Click += new System.EventHandler(this.btnAcceder_Click_1);
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(341, 377);
+            this.btnSalir.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(53, 288);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
-            this.btnSalir.TabIndex = 20;
+            this.btnSalir.Size = new System.Drawing.Size(215, 41);
+            this.btnSalir.TabIndex = 6;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.Location = new System.Drawing.Point(79, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(159, 44);
+            label1.TabIndex = 0;
+            label1.Text = "LOGUIN";
             // 
             // empleadosBindingSource
             // 
@@ -100,17 +120,20 @@
             // 
             // Loguin
             // 
+            this.AcceptButton = this.btnAcceder;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(656, 582);
+            this.ClientSize = new System.Drawing.Size(325, 388);
+            this.Controls.Add(label1);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnAcceder);
             this.Controls.Add(contraseniaLabel);
-            this.Controls.Add(this.contraseniaTextBox);
+            this.Controls.Add(this.txtContra);
             this.Controls.Add(usuarioLabel);
-            this.Controls.Add(this.usuarioTextBox);
+            this.Controls.Add(this.txtUsuario);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Loguin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Loguin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.empleadosBindingSource)).EndInit();
@@ -122,8 +145,8 @@
         #endregion
 
         private System.Windows.Forms.BindingSource empleadosBindingSource;
-        private System.Windows.Forms.TextBox contraseniaTextBox;
-        private System.Windows.Forms.TextBox usuarioTextBox;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.Button btnAcceder;
         private System.Windows.Forms.Button btnSalir;
     }
--- MiniSuper/MiniSuper/Loguin.cs
+++ MiniSuper/MiniSuper/Loguin.cs
@@ -26,43 +26,68 @@ namespace MiniSuper
 
         }
 
+        void Limpiar()
+        {
+            txtUsuario.Text = "";
+            txtContra.Text = "";
+        }
       
         private void btnAcceder_Click_1(object sender, EventArgs e)
         {
-            using (MiniSuperEntities bd = new MiniSuperEntities())
+            if(txtUsuario.Text.Equals("") || txtContra.Text.Equals(""))
             {
-                var lista = from em in bd.Empleados
-                            where em.contrasenia == contraseniaTextBox.Text
-                            && em.usuario == usuarioTextBox.Text
-                            select em;
-
-                if (lista.Count() > 0)
+                MessageBox.Show("¡Todos los campos son obligatorios!", "Completar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                using (MiniSuperEntities bd = new MiniSuperEntities())
                 {
-                    var cargo = from emp in bd.Empleados
-                                where emp.usuario == usuarioTextBox.Text
+                    var lista = from em in bd.Empleados
+                                where em.contrasenia == txtContra.Text
+                                && em.usuario == txtUsuario.Text
+                                select em;
 
-                                select new
-                                {
-                                    emp.idCargo
-                                };
-
-                    foreach (var iterar in cargo)
+                    if (lista.Count() > 0)
                     {
-                        if(iterar.idCargo == 1)
+                        var cargo = from emp in bd.Empleados
+                                    where emp.usuario == txtUsuario.Text
+
+                                    select new
+                                    {
+                                        emp.idCargo
+                                    };
+
+                        foreach (var iterar in cargo)
                         {
-                            MessageBox.Show("Funciona, su cargo es: Gerente");
-                        }
-                        else if(iterar.idCargo == 2)
-                        {
-                            MessageBox.Show("Funciona, su cargo es: Cajero");
+                            if (iterar.idCargo == 1)
+                            {
+                                MessageBox.Show("Funciona, su cargo es: Gerente");
+                                Limpiar();
+                            }
+                            else if (iterar.idCargo == 2)
+                            {
+                                MessageBox.Show("Funciona, su cargo es: Cajero");
+                                Limpiar();
+                            }
                         }
                     }
-                }
-                else
-                {
-                    MessageBox.Show("El usuario no existe.");
-                }
+                    else
+                    {
+                        MessageBox.Show("¡El usuario ingresado no existe!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Limpiar();
+                    }
 
+                }
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("¿Estás seguro que quieres salir?", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+
+            if(result == DialogResult.OK)
+            {
+                Application.Exit();
             }
         }
     }
